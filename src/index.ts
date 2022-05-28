4fbbdb2ecbaffc1c2ce5c55f45d569753663a1ee
tiagostn <tiagostn@senado.leg.br>
2022-05-28 18:49:35 -0300

    add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ info: 'OK!' });
-});
+app.get('/', (request, response) => response.json({ info: 'OK!' }));
 
-app.get('/info', (request, response) => {
-  return response.json({ version: '1.0' });
-});
+app.get('/info', (request, response) => response.json({ version: '1.0' }));
 
 app.listen(3030);
+// eslint-disable-next-line no-console
 console.log('>> http://localhost:3030');
