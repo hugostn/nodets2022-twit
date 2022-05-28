6e2db60d044d08f6e890470194817d74b0d0d6b1
tiagostn <tiagostn@senado.leg.br>
2022-05-28 16:37:57 -0300

    add dev watch mode

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,9 @@ app.get('/', (request, response) => {
     return response.json({ info: 'OK!' });
 });
 
+app.get('/info', (request, response) => {
+  return response.json({ version: '1.0' });
+});
+
 app.listen(3030);
 console.log('>> http://localhost:3030');
